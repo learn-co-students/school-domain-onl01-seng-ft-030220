d38bcaadc7538e43640b7ff7a469ae09ac048af1
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-11 14:23:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster, :student_name, :school_name 
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new{ |h, k| h[k] = [] }  
+  end
+
+  def add_student(student_name, grade)
+      @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    @grade=grade
+  end 
+  
+  def grade (level) 
+      @roster.select! {|grade| grade==level}.values.flatten    
+  end 
+  
+  def sort  #sort by grade
+   @roster.each do |grade, names|
+     names.sort!
+    #binding.pry
+  end
+end
+end
